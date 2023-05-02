78d6471c8e9b6b3fe9cd65cc2871254d51787df3
Itayos <66430298+ItayosP@users.noreply.github.com>
2023-05-02 19:07:43 +0300

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+figlet -c "Neros" | lolcat
+echo "                            By: Itayos   Version: 1.0" | lolcat
+
+while true
+do
+        #To show the TIME of the User
+        time=`date +"%T"`
+        echo -ne " ${time}"\\r
+done
